4a43ea619ed9fc74b42cf2dd99b9a98b9eb4d5f5
Forbes Lindesay <forbes@lindesay.co.uk>
2015-05-17 18:51:32 +0100

    Get &attributes working properly
    Also updates the deps and fixes a few other unrelated issues (I should
    probably have done lots of separate pull requests but oh well)
    [closes #47][closes #46]

--- test/mock-dom.js
+++ test/mock-dom.js
@@ -19,27 +19,35 @@ function mock() {
   function mockFor(name) {
     return function (attribs) {
       var children = Array.prototype.slice.call(arguments, 1);
+      var sortedAttribs = {};
       if (attribs) {
-      if ('class' in attribs) throw new Error('Cannot have an attribute named "class", perhaps you meant "className"');
-      if ('className' in attribs) {
-        attribs['class'] = attribs.className;
-        delete attribs.className;
-      }
-      if (attribs['class'] === '') delete attribs['class'];
-      Object.keys(attribs).forEach(function (key) {
-        if (attribs[key] === true) {
-          attribs[key] = key;
-        } else if (attribs[key] === false || attribs[key] === null || attribs[key] === undefined) {
-          delete attribs[key];
-        } else {
-          attribs[key] = attribs[key] + '';
+        if ('class' in attribs) throw new Error('Cannot have an attribute named "class", perhaps you meant "className"');
+        if ('className' in attribs) {
+          attribs['class'] = attribs.className;
+          delete attribs.className;
         }
-      });
+        if (attribs['class'] === '') delete attribs['class'];
+        if (attribs['style']) {
+          if (typeof attribs['style'] !== 'object') {
+            throw new Error('Cannot have anything other than an object as the "style"');
+          }
+          attribs['style'] = Object.keys(attribs.style).sort().map(function (key) {
+            return key + ':' + attribs['style'][key];
+          }).join(';');
+        }
+        Object.keys(attribs).sort().forEach(function (key) {
+          if (attribs[key] === true) {
+            sortedAttribs[key] = key;
+          } else if (attribs[key] === false || attribs[key] === null || attribs[key] === undefined) {
+          } else {
+            sortedAttribs[key] = attribs[key] + '';
+          }
+        });
       }
       return {
         type: 'tag',
         name: name,
-        attribs: attribs,
+        attribs: sortedAttribs,
         children: Array.isArray(children) ? children : (children ? [children] : [])
       };
     }
